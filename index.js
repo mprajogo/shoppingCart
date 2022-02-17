17c90f61aed1ec71680baf394f71c9e6a927d863
Michelle Prajogo <mprajogo@gmail.com>
2022-02-17 13:09:03 +1100

    add remove button to cart

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Initialise starting cart price 
 let totalPrice = 0;
 
+//Initialise empty cart array
 let cartArray = [];
 
 // Display current cart total
@@ -20,18 +21,54 @@ var addToCart = function() {
         totalPrice += itemPrice;
         // Display cart total
         updateCart();
-
         
-        cartArray.push(`<li class="list-group-item">${itemName} | $${itemPrice} </li>`);
+        cartArray.push(
+            `<li class="list-group-item">
+                <div class="row">
+                    <div class="col">
+                        ${itemName} | $${itemPrice} 
+                    </div>
+                    <div class="col">
+                        <button class="btn btn-outline-secondary delete-button" data-price="${itemPrice}" data-name="${itemName}">Remove from cart</button>
+                    </div>
+                </div>
+            </li>`);
         document.querySelector('#cartContents').innerHTML = cartArray.join('');
       }
     }
   }
 
+
+//// Remove item from cart 
+
+// var removeFromCart = function() {
+//     document.onclick = function(e) {
+//       if (e.target.className == 'delete-button') {
+//         var itemPrice = Number(e.target.getAttribute("data-price"));
+//         var itemName = e.target.getAttribute("data-name");
+//         //Increase cart total 
+//         totalPrice -= itemPrice;
+//         // Display cart total
+//         updateCart();
+        
+//         const index = cartArray.indexOf(
+//             `<li class="list-group-item">
+//                 <div class="row">
+//                     <div class="col">
+//                         ${itemName} | $${itemPrice} 
+//                     </div>
+//                     <div class="col">
+//                         <button class="btn btn-outline-secondary delete-button" data-price="${itemPrice}" data-name="${itemName}">Remove from cart</button>
+//                     </div>
+//                 </div>
+//             </li>`);
+//         if (index > -1) {
+//         cartArray.splice(index, 1); // 2nd parameter means remove one item only
+//         }   
+//       }
+//     }
+//   }
+
 addToCart();
-
-//// Add  cart item to new array 
-
-
-
+// removeFromCart();
 
